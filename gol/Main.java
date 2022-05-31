18443c2aa542bb1840407b6376d55f95a11fa832
joeru <102632048+joeru-bann@users.noreply.github.com>
2022-05-31 20:18:43 +1200

    closing by saying "stop" (not working yet)

--- gol/Main.java
+++ gol/Main.java
@@ -1,14 +1,33 @@
 package gol;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import java.awt.Dimension;
+
 
 /**
  * @Joel
  * @26/05/22
  */
 public class Main {
-
+	boolean end = false;
+	 private static JFrame window;
+		    	
+		    	
     public static void main(String[] args) {
-        Window.create();
-        
-        new MainLoop().start();
-    }
+		 {
+			new Game(); //immediately goes to Game class to start
+		        
+		       Scanner keyin = new Scanner (System.in);
+               String halt = keyin.nextLine().toLowerCase(); //automatically makes lower case for ease
+		       if (halt == "stop") {
+		    	   window.dispose();
+		    	   window.setVisible(false);
+		       }
+		       
+		        			        		
+		      }	
+    	}
 }
+    	 	
+         
