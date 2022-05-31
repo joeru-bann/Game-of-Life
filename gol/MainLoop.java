651de8387935a2f3593225ed8103f4ee26bd3be9
joeru <102632048+joeru-bann@users.noreply.github.com>
2022-05-31 23:18:56 +1200

    quitting now works

--- gol/MainLoop.java
+++ gol/MainLoop.java
@@ -4,10 +4,7 @@ import javax.swing.Timer;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.Scanner;
-import java.io.File;
 import java.io.IOException;
-import gol.Game;
-import java.awt.Dimension;
 
 /**
  * Looping updates with delayed timer
@@ -15,29 +12,45 @@ import java.awt.Dimension;
  * @Joel Bannister
  * @26/05/22
  */
-
 public class MainLoop implements ActionListener
 {
 	boolean rateChosen = false;
     private World world;
     private Timer timer;
-    boolean gameStarted;
-    public MainLoop()
-    {
+    static boolean gameStarted;
+    private JFrame window;
+    
+    public MainLoop(){
         this.world = new World();
         this.timer = new Timer(Refer.refresh_rate, this);
+        
         }
     
 
     public void start(){
         this.timer.start(); //timer begins
-        gameStarted = true;
-    }
+         gameStarted = true;
+         while (gameStarted) {
+            Scanner kb = new Scanner (System.in);
+            	String endCmd = kb.nextLine();
+            	if (endCmd == "quit") {
+            		System.out.print("quit test");
+            	}
+            	else {
+            		System.out.println("quitting...");
+            		Window.dispose();
+            		gameStarted = false;
+            		break;
+            	}
+             
+         }
+       }
     
     @Override //overide for actionlistener
     public void actionPerformed(ActionEvent e){
         Window.updateBoard(world);
         this.world.update();
+        
           
     		}
     
