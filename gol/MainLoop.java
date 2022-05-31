f98afaa384b2d23a265d3b46db5b2b760ecef35d
joeru <102632048+joeru-bann@users.noreply.github.com>
2022-05-31 20:13:13 +1200

    working with new refresh

--- gol/MainLoop.java
+++ gol/MainLoop.java
@@ -1,34 +1,44 @@
 package gol;
+import javax.swing.JFrame;
 import javax.swing.Timer;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import gol.Game;
+import java.awt.Dimension;
 
-import gol.Window;
-import gol.World;
-import gol.Refer;
 /**
  * Looping updates with delayed timer
  *
  * @Joel Bannister
  * @26/05/22
  */
+
 public class MainLoop implements ActionListener
 {
+	boolean rateChosen = false;
     private World world;
     private Timer timer;
+    boolean gameStarted;
     public MainLoop()
     {
         this.world = new World();
-        this.timer = new Timer(Refer.TIMER_DELAY, this);
-    }
+        this.timer = new Timer(Refer.refresh_rate, this);
+        }
+    
 
     public void start(){
         this.timer.start(); //timer begins
+        gameStarted = true;
     }
     
     @Override //overide for actionlistener
     public void actionPerformed(ActionEvent e){
         Window.updateBoard(world);
         this.world.update();
-    }
+          
+    		}
+    
 }
