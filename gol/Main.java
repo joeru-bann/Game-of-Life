32a3637809c835e942170a4bc868c833461b435b
joeru <102632048+joeru-bann@users.noreply.github.com>
2022-05-31 23:21:36 +1200

    Update Main.java

--- gol/Main.java
+++ gol/Main.java
@@ -1,33 +1,19 @@
 package gol;
-import java.util.Scanner;
-
-import javax.swing.JFrame;
-import java.awt.Dimension;
 
 
 /**
  * @Joel
  * @26/05/22
+ * starting and ending the game
  */
 public class Main {
 	boolean end = false;
-	 private static JFrame window;
 		    	
 		    	
     public static void main(String[] args) {
 		 {
 			new Game(); //immediately goes to Game class to start
-		        
-		       Scanner keyin = new Scanner (System.in);
-               String halt = keyin.nextLine().toLowerCase(); //automatically makes lower case for ease
-		       if (halt == "stop") {
-		    	   window.dispose();
-		    	   window.setVisible(false);
-		       }
-		       
-		        			        		
-		      }	
-    	}
-}
-    	 	
-         
+		    }
+	   	        		
+    	}	
+}        
