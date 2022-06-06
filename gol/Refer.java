c70a1a17e235b7e0437533625539343137b89881
joeru <102632048+joeru-bann@users.noreply.github.com>
2022-06-07 11:00:23 +1200

    Update Refer.java

--- gol/Refer.java
+++ gol/Refer.java
@@ -1,17 +1,21 @@
-package gol;
+    package gol;
 /**
  * definitions for world
  *
  * @Joel Bannister
  * 25/05
  */
-	
+    
 public class Refer {
 
     public static final int world_width = 250;
     public static final int world_height = 150;
     
+    public static final int customWorld_width =  Game.cols;
+    public static final int customWorld_height = Game.rows;
+    
     public static final int cell_size = 3; //cell size compared to grid  
 
+    
     public static int newRate = Game.refresh;
 }
