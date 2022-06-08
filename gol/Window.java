bf1c7406cedcbd4c7ede15cff78f4adbc048ee7d
joeru <102632048+joeru-bann@users.noreply.github.com>
2022-06-08 17:12:29 +1200

    working pausing function

--- gol/Window.java
+++ gol/Window.java
@@ -1,11 +1,20 @@
 package gol;
 
 import java.awt.Dimension;
+import java.util.Scanner;
+
 import javax.swing.JFrame;
+
 public class Window {
 
     private static JFrame window;
     private static Board board;
+    
+    public static void sleep(int time) { // function to help reading process be smoother experience by pausing
+    	try {
+    		Thread.sleep(time);
+    		} catch (Exception e) {}
+    }
 
     public static void create() {
         window = new JFrame("Joel's Game of Life");
@@ -20,7 +29,9 @@ public class Window {
         window.setContentPane(board); // makes board the child component
         window.pack(); //window fits size of subcompenents
         window.setVisible(true);
+        
     }
+    
 
     public static void updateBoard(World renderWorld) {
         board.update(renderWorld);
@@ -31,9 +42,37 @@ public class Window {
 		window.dispose(); 
 		System.exit(0);
 	}
+	
+	
+	
 		
 	public static void pause (){ //pausing game when generations have completed
 		
-		
-	}
+	            Scanner kb = new Scanner (System.in);
+
+         		System.out.println("All " + Game.gens + " generations, have completed.");
+         		
+         		
+         		System.out.println("new game? type (new), exit game: type (quit) or (q)");
+         		String choice = kb.nextLine().toLowerCase();
+         	         		
+         		if (choice.equals("new")) {
+         			System.out.println("creating new game...");
+         			sleep(3000);
+         			new Main();
+         		}
+         			else if (choice.equals("quit") || choice.equals("q")){
+             			System.out.println("quitting...");
+         				Window.dispose();
+ 	            		System.exit(0);
+ 	            		window.setVisible(false);
+ 	            		MainLoop.endTimer = true;
+         			}
+         		
+         			else if (!choice.equals("quit") || !choice.equals("new") || !choice.equals("q")){
+         				System.out.println("please choice an option from above");
+         				
+         			}
+ 	 }
+	
 }
