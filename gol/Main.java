769a9bdf8a293d150d60c4409212a38045b18c35
joeru <102632048+joeru-bann@users.noreply.github.com>
2022-07-26 10:53:54 +1200

    sound directory + start when beginning game

--- gol/Main.java
+++ gol/Main.java
@@ -1,19 +1,22 @@
 package gol;
 
-
 /**
  * @Joel
  * @26/05/22
  * starting and ending the game
  */
-public class Main {
-	boolean end = false;
+public class Main {		 
+	
 		    	
-		    	
-    public static void main(String[] args) {
-		 {
-			new Game(); //immediately goes to Game class to start
-		    }
-	   	        		
-    	}	
-}        
+    public static void main(String[] args){ //beginning music and starting the Game class with intro to gol (game of life)
+		 
+			String filepath = "Street Party.wav"; 
+  
+			Sound musicObject = new Sound();
+			musicObject.playMusic(filepath);
+			
+			new Game(); //redirecting to introduction
+    
+    	}
+    }
+       
