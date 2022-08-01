d99640378a554e051d0f14350fc70a78f5d61753
joeru <102632048+joeru-bann@users.noreply.github.com>
2022-08-01 12:11:24 +1200

    random music

--- gol/Main.java
+++ gol/Main.java
@@ -1,22 +1,34 @@
 package gol;
-
+import java.io.File;
+import java.util.Random;
+import java.util.Scanner;
 /**
  * @Joel
  * @26/05/22
- * starting and ending the game
+ * starting game + with music
  */
-public class Main {		 
-	
-		    	
+public class Main {
+
+		
     public static void main(String[] args){ //beginning music and starting the Game class with intro to gol (game of life)
-		 
-			String filepath = "Street Party.wav"; 
-  
+
+    	String[] arr = {"Street Party", "Tropical Carnival", "Lost Woods"};
+        Random rand = new Random();
+
+        // randomly selects an index from the arr
+        int select = rand.nextInt(arr.length); 
+    	String noWav = arr[select] + ".wav";
+    	
+    	
+    	String filepath = noWav;
+    		
+    	System.out.println("now playing: " + arr[select]);
 			Sound musicObject = new Sound();
 			musicObject.playMusic(filepath);
 			
 			new Game(); //redirecting to introduction
-    
     	}
-    }
+    	
+    }	
+
        
