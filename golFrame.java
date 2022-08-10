09d97d0be74a8c6229081f9186bb971eab6f2b11
joeru <102632048+joeru-bann@users.noreply.github.com>
2022-08-10 20:25:52 +1200

    clean up + square 800x800 size

--- golFrame.java
+++ golFrame.java
@@ -1,47 +1,27 @@
 package jolgol;
-
-import java.util.Random;
-
-//Joel game of life - frame
-
-
 //An extended version of java.awt.Frame that adds support for the JFC/Swing component architecture:
 //JFrama -> the frame of the canvas
+//you always need it to be able to show stuff
 import javax.swing.JFrame;
 
+import jolgol.golPanel;
+
 public class golFrame extends JFrame{
-	private static final long serialVersionUID = 1L;
+ private static final long serialVersionUID = 1L;
 
 	public golFrame() {
 		add(new golPanel());
 		
-		setSize (1300, 700); //width, height
+		//setting the size: width=700; height=1300
+		setSize (800, 800);
 		
-		setVisible (true); //board becomes visible
+		//we want to see the board, so we set it to visible - true:
+		setVisible (true); 
 		
 		//setting default: when we close our program, all things shut down:
 		setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		
 	}
-	public static void main(String[] args) {
-		
-//		String[] arr = {"Street Party", "Tropical Carnival", "Lost Woods"};
-//        Random rand = new Random();
-//
-//        // randomly selects an index from the arr
-//        int select = rand.nextInt(arr.length); 
-//    	String noWav = arr[select] + ".wav";
-//    	
-//    	
-//    	String filepath = noWav;
-//    		
-//    	System.out.println("now playing: " + arr[select]);
-//			Sound musicObject = new Sound(); 
-//			musicObject.playMusic(filepath);
-			
-			new golPanel(); //redirecting
 
-    	}
-	
 }
-
+	
