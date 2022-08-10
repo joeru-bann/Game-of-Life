5cc392a2206e0431c263bd2b9039e876534a168b
joeru <102632048+joeru-bann@users.noreply.github.com>
2022-08-10 20:24:23 +1200

    cleaning up / shortening unused old options

--- golIntro.java
+++ golIntro.java
@@ -1,13 +1,18 @@
 package jolgol;
 
 
+import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class golIntro {
 	
+	//private static final long serialVersionUID = 1L;
+
+	
     String modeChoice;
     String intro;
+    String c;
     boolean introChosen = false;
     boolean gameModePicked = false;
     boolean fileBoard = false;
@@ -26,79 +31,62 @@ public class golIntro {
         	char c = output.charAt(i);
             System.out.print(c);
             try {
-                TimeUnit.MILLISECONDS.sleep(20);
+                TimeUnit.MILLISECONDS.sleep(10);
               }
               catch (Exception e) {}
         }
     }
     
-    
-	public void Intro() {
+    public static void main(String[] args) {
 		
-	   Scanner keyin = new Scanner(System.in); // reading inout from the user
+		String[] arr = {"Street Party", "Tropical Carnival", "Lost Woods"};
+        Random rand = new Random();
 
+        // randomly selects an index from the arr
+        int select = rand.nextInt(arr.length); 
+    	String noWav = arr[select] + ".wav";
+    	
+    	String filepath = noWav;
+    		
+    	System.out.println("now playing: " + arr[select]);
+			Sound musicObject = new Sound();
+			musicObject.playMusic(filepath);
+		
+			new golIntro();
+
+	}
+    
+	public golIntro() {
+		
+		   Scanner keyin = new Scanner(System.in); // reading inout from the user
 	 System.out.println("read introduction? or skip:  type r/read, or s/skip");
+
      while (!introChosen){
          intro = keyin.nextLine().toLowerCase(); 
-         keyin.close(); //preventing input leak
-         
+
          if(intro.equals("read") || (intro.equals("r"))) {
              introChosen = true;
              timedPrint("Heeeelloo!.. today, I introduce to you Joel's Game of Life! \n" 
                  + "How it goes: \n"
                  +"You can choose randomized, which spawns a random map of cells; either dead or alive, \n"
-                 +"Board 1/2/3, all of which are pre-made boards created for you that look pretty cool \n"
-                 +"Potentially, you can also load-up your own map of 1's and 0's. This will turn into a board of cells \n" );
-            sleep(3000);
-            timedPrint("What do you feel like playing? \n");
-            sleep(200);
-            timedPrint("Randomized type \"r\" or \"random\",\n");
-            sleep(200);
-            timedPrint("Your own map seed? type \"File\" \n");
+                 +"You can also load-up your own map of 1's and 0's. This will turn into a board of cells \n" );
+          
+             System.out.println("press \"c\" to continue");
+             c = keyin.nextLine().toLowerCase();
+             	if(c.equals("c")) {
+             		new golFrame();
+             	}
          } 
          
          else if (intro.equals("skip") || (intro.equals("s"))) {
              introChosen = true;
-         
-             timedPrint("What do you feel like playing? \n");
-             sleep(200);
-             timedPrint("Randomized: type \"r\" or \"random\"\n");
-             sleep(200);
-             timedPrint("Your own map seed: type \"File\" \n");
+             
+             new golFrame();
          }
          else {
              System.out.print("please choose an intro option..");
-             introChosen = false;
-         }
-     }
-
-     while(!gameModePicked){
-       
-
-         modeChoice=keyin.nextLine().toLowerCase(); 
-         if(modeChoice.equals("random") || (modeChoice.equals("r"))){
-             gameModePicked = true;
-             choice = 1;
-         }
-         else if (modeChoice.equals("file") || (modeChoice.equals("f"))){ 
-             gameModePicked = true;
-             fileBoard = true;
-             choice = 2;
-         }
-         else {
-             System.out.println("Please choose a valid option from above");
-         }
-
-     }
-     switch (choice){
-         case 1: //random generation
-         	
-         System.out.println("what alive-cell colour do you want?");
-         String aColour = keyin.nextLine().toLowerCase();
-         System.out.println("what dead-cell colour do you want?");
-         String dColour = keyin.nextLine().toLowerCase();
-         
-              new golFrame();
-         }
+         	}
+      }   
+	}
  }
-}
+
