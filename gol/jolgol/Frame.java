d562a480d3d29fa8155078e15d0c68f91da30ae3
joeru <102632048+joeru-bann@users.noreply.github.com>
2022-08-27 21:44:34 +1200

    added JFrame, named "Joel's Game of Life" (working)

--- gol/jolgol/Frame.java
+++ gol/jolgol/Frame.java
@@ -2,23 +2,27 @@ package jolgol;
 //JFrame -> the frame of the canvas
 import javax.swing.JFrame;
 
+
 public class Frame extends JFrame{
  private static final long serialVersionUID = 1L;
 
-	public Frame() {
-		add(new Panel());
-		
-		//setting the size: width=700; height=1300
-		setSize (800, 800);		
-				
-		setLocationRelativeTo(null); //setting window to be middle by default
-		
-		setVisible (true);	//we want to see the board, so we set it to visible - true:
+ 	private static JFrame frame;
 
-		setAlwaysOnTop(true); //setting the window to be the "top" tab/front tab
+	public Frame() {
+        frame = new JFrame("Joel's Game of Life");
+
+		frame.add(new Panel());
+		
+		//setting the size: 
+		frame.setSize(800, 800); //square dimensions
+				
+		frame.setLocationRelativeTo(null); //setting window to be middle by default
+		
+		frame.setVisible (true);	//we want to see the board, so we set it to visible - true:
+
+		frame.setAlwaysOnTop(true); //setting the window to be the "top" tab/front tab
 				
 		//setting default: when we close our program, all things shut down:
-		setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 	}
-
 }
